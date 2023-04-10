9664aa14181e88aab89bf148c5e300e19ad9ebad
Eslam-Youness <eslam-youness@elarabygroup.com>
2023-04-10 03:42:38 +0200

    creating notification service class with custom layout for notification

---
+++ app/src/main/java/com/example/fcmnotification/FirebaseMessagingService.kt
@@ -0,0 +1,12 @@
+package com.example.fcmnotification
+
+class FirebaseMessagingService :com.google.firebase.messaging.FirebaseMessagingService() {
+
+    /*
+    generate the notification
+    attach the notification created with custom layout
+    show the notification
+     */
+
+
+}
